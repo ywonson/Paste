c174ab7fe2d7127405c81872f2ceb8f0fdebe8a0
ywonson <ywson51@gmail.com>
2023-06-22 22:53:33 +0900

    txt file into clipborad pass with enter

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,48 @@
-use iced::Settings;
-use iced::widget::{Button, Column, Container, Text};
-use iced::Sandbox;
+//extern crate clipboard;
+use clipboard::ClipboardProvider;
+use clipboard::ClipboardContext;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::io;
 
-fn main() -> Result<(), iced::Error> {
-    Counter::run(Settings::default())
-}
+fn read_file_to_vector(filename: &str) -> Vec<String> {
+    let file = File::open(filename).expect("File not found");
+    let reader = BufReader::new(file);
 
-struct Counter {
-    count: i32,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum CounterMessage {
-    Increment,
-    Decrement,
-}
-
-impl Sandbox for Counter {
-    type Message = CounterMessage;
-
-    fn new() -> Self {
-        Counter { count: 0 }
+    let mut lines = Vec::new();
+    for line in reader.lines() {
+        let line = line.expect("Could not parse line");
+        lines.push(line);
     }
 
-    fn title(&self) -> String {
-        String::from("Counter app")
-    }
+    return lines;
+}
 
-    fn update(&mut self, message: Self::Message) {
-        match message {
-            CounterMessage::Increment => self.count += 1,
-            CounterMessage::Decrement => self.count -= 1,
+fn main() {
+    let lines = read_file_to_vector("my_file.txt");
+
+    let mut ctx: ClipboardContext = ClipboardProvider::new().expect("Failed to initialize clipboard");
+    let mut index = 0;
+    let mut formatted_line = String::new();
+
+    loop {
+        if index >= lines.len() {
+            // Erase clipboard contents and exit the loop
+            ctx.set_contents(String::new()).expect("Failed to set clipboard contents");
+            break;
+        }
+
+        
+        formatted_line = format!("my name is {}", lines[index]);
+        ctx.set_contents(formatted_line.clone()).expect("Failed to set clipboard contents");
+        println!("{}", lines[index]);
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+
+        // Check if Enter or some key was pressed
+        if input.trim().is_empty() {
+            index += 1;
         }
     }
-
-    fn view(&self) -> iced::Element<Self::Message> {
-        let label = Text::new(format!("Count: {}", self.count));
-        let incr = Button::new("Increment").on_press(CounterMessage::Increment);
-        let decr = Button::new("Decrement").on_press(CounterMessage::Decrement);
-        let col = Column::new().push(incr).push(label).push(decr);
-        Container::new(col).center_x().center_y().width(iced::Length::Fill).height(iced::Length::Fill).into()
-    }
 }
-
-//https://nikolish.in/gs-with-iced-1
